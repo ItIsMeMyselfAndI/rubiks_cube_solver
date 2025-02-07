9bdc355c274e622832a384199fe01b2cf3e74c0c
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-02-07 10:45:20 +0800

    edge solution execution was already working from last update, the corner solution execution was buggy instead. it's now fixed. no buggs so far.

--- corners.py
+++ corners.py
@@ -38,7 +38,7 @@ class Corners(cube.Cube, inspection.Inspect):
             "i":"F R' R U' R' U' R U R' F' R U R' U' R' F R R F'",
             "j":"R' R U' R' U' R U R' F' R U R' U' R' F R R",
             "k":"R' D' R U' R' U' R U R' F' R U R' U' R' F R D R",
-            "l":"F2 R R U' R' U' R U R' F' R U R' U' R' F R R' F2",
+            "l":"F2 R' R U' R' U' R U R' F' R U R' U' R' F R R F2",
             "m":"F R U' R' U' R U R' F' R U R' U' R' F R F'",
             "n":"R' F R U' R' U' R U R' F' R U R' U' R' F R F' R",
             "o":"R2' F R U' R' U' R U R' F' R U R' U' R' F R F' R2",
@@ -135,7 +135,8 @@ if __name__ == "__main__":
     my_cube = cube.Cube()
     move = movement.Move()
 
-    scramble_algorithm = "L2 F B L F' B2 L' R' F' L U' B2 R' F D2 R' F' B2 R2 D2 B F D2 U2 R2 D' B2 L F' B'"
+    scramble_algorithm = "L' R U' D' R2 B D F U' B2 D L U R L2 U F2 B' L2 D2 B' R U L2 F R' D U2 B2 D2"
+    # scramble_algorithm = "L2 F B L F' B2 L' R' F' L U' B2 R' F D2 R' F' B2 R2 D2 B F D2 U2 R2 D' B2 L F' B'"
     # scramble_algorithm = "U' B2 U L2 D L2 R2 D' B' R D' L R' B2 U2 F' L' U'"
     # scramble_algorithm = "U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2"
     # scramble_algorithm = "R L B R F' B' D' F2 L2 U2 R' F2 U' B' F L R2 B F' R' B' L2 U2 R2 B' L2 U' F' L U"
@@ -150,12 +151,14 @@ if __name__ == "__main__":
     my_cube.display()
     corners.inspect("era", enable_parity=False)
     print(corners.pieces_sequence)
-    letters_sequence = corners.getLettersSequence()
-    print(" ".join(letters_sequence))
-    solution = corners.getSolution(letters_sequence)
+    corners_sequence = corners.getLettersSequence()
+    print(" ".join(corners_sequence))
+    corner_solution = corners.getSolution(corners_sequence)
     print(corners.is_parity)
     print()
-    move.executeAlgorithm(" ".join(solution), my_cube.faces)
+    move.executeAlgorithm(" ".join(corner_solution), my_cube.faces)
     print()
     my_cube.display()
     print()
+    for algorithm in corner_solution:
+        print(algorithm)
--- main.py
+++ main.py
@@ -6,94 +6,40 @@ import corners
 
 def main():
     my_cube = cube.Cube()
+    my_cube.display()
     move = movement.Move()
     
     # generate scramble
     new_scramble = scramble.Scramble()
     new_scramble.generateAlgorithm()
-    # scramble_algorithm = " ".join(new_scramble.algorithm)
-    scramble_algorithm = "L2 F B L F' B2 L' R' F' L U' B2 R' F D2 R' F' B2 R2 D2 B F D2 U2 R2 D' B2 L F' B'"
-    
-    # execute scramble
+    scramble_algorithm = " ".join(new_scramble.algorithm)
+    # scramble_algorithm = "L' R U' D' R2 B D F U' B2 D L U R L2 U F2 B' L2 D2 B' R U L2 F R' D U2 B2 D2"
+    # scramble_algorithm = "L2 F B L F' B2 L' R' F' L U' B2 R' F D2 R' F' B2 R2 D2 B F D2 U2 R2 D' B2 L F' B'"
+    # scramble_algorithm = "U' B2 U L2 D L2 R2 D' B' R D' L R' B2 U2 F' L' U'"
+    # scramble_algorithm = "U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2"
+    # scramble_algorithm = "R L B R F' B' D' F2 L2 U2 R' F2 U' B' F L R2 B F' R' B' L2 U2 R2 B' L2 U' F' L U"
+    # scramble_algorithm = "R2 U2 R2 U2 R2 U2"
+    print(scramble_algorithm)
     move.executeAlgorithm(scramble_algorithm, my_cube.faces)
     my_cube.display()
+
     
+    # inspect edges 
     my_edges = edges.Edges(my_cube.faces, ["uk", "ku"], 22)
-    my_corners = corners.Corners(my_cube.faces, ["aer", "era", "rae"], 21)
-    my_cube.display()
-    print()
-
     my_edges.inspect("uk", enable_parity=True)
-    my_corners.inspect("era", enable_parity=True)
-    
-    print(my_edges.pieces_sequence)
-    print(my_corners.pieces_sequence)
-    
     edges_sequence = my_edges.getLettersSequence()
-    corners_sequence = my_corners.getLettersSequence()
-    
-    print(" ".join(edges_sequence))
-    print(" ".join(corners_sequence))
     edge_solution = my_edges.getSolution(edges_sequence)
+    # inspect corners 
+    my_corners = corners.Corners(my_cube.faces, ["aer", "era", "rae"], 21)
+    my_corners.inspect("era", enable_parity=False)
+    corners_sequence = my_corners.getLettersSequence()
     corner_solution = my_corners.getSolution(corners_sequence)
-    print(my_edges.is_parity)
-    print(my_corners.is_parity)
-    print()
-    
-    for algorithm in edge_solution:
-        print(algorithm)
-        move.executeAlgorithm(algorithm, my_cube.faces)
-    print()
+
+    # execute solutions
+    move.executeAlgorithm(" ".join(edge_solution), my_cube.faces)
     my_cube.display()
-    for algorithm in corner_solution:
-        print(algorithm)
-        move.executeAlgorithm(algorithm, my_cube.faces)
-    print()
+    move.executeAlgorithm(" ".join(corner_solution), my_cube.faces)
     my_cube.display()
-
-
-    # # scramble_algorithm = "B' F' R2 U2 B F' U2 L' F' D2 U' L2 R' F2 D B F' D L2 B R' L2 D2 U B' R2 L' B2 R2 L'"
-    # # scramble the cube
-    # move.executeAlgorithm(scramble_algorithm, my_cube.faces)
-
-    # # inspect edges 
-    # my_edges = edges.Edges(my_cube.faces, ["uk", "ku"], 22)
-    # my_edges.inspect("uk", enable_parity=True)
-    # edges_sequence = my_edges.getLettersSequence()
-    # edge_solution = my_edges.getSolution(edges_sequence)
-    # # inspect corners
-    # my_corners = corners.Corners(my_cube.faces, ["aer", "era", "rae"], 21)
-    # my_corners.inspect("era", enable_parity=False)
-    # corners_sequence = my_corners.getLettersSequence()
-    # corner_solution = my_corners.getSolution(corners_sequence)
-
-
-    # print("[Scrambled Cube]")
-    # print()
-    # my_cube.display()
-    # print()
-    # print(f"\tScramble: {scramble_algorithm}")
-    # print()
-    # print("[Solution Sequence]")
-    # print()
-    # print(my_edges.pieces_sequence)
-    # print(f"\tEdges: {" ".join(edges_sequence)}")
-    # print(f"\tParity: {my_edges.is_parity}")
-    # print(my_edges.pieces_sequence)
-    # print(f"\tCorners: {" ".join(corners_sequence)}")
-    # print()
-    # print("[Solved Cube]")
-    # print()
-    # for algorithm in edge_solution:
-    #     move.executeAlgorithm(algorithm, my_cube.faces)
-    # my_cube.display()
-    # print()
-    # for algorithm in corner_solution:
-    #     move.executeAlgorithm(algorithm, my_cube.faces)
-    # my_cube.display()
-    # print()
-    # print(f"\tEdges: {my_edges.solved_pieces}")
-    # print(f"\tCorners: {my_corners.solved_pieces}")
 
 
 
